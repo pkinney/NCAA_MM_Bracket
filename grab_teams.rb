58e5e7838292d29458b890750f252908aac743bc
Powell Kinney <pkinney@gmail.com>
2011-03-13 13:08:40 -0500

    Updated the grab_team.rb for 2011

--- grab_teams.rb
+++ grab_teams.rb
@@ -1,29 +1,25 @@
 require 'net/http'
 
-# http://web1.ncaa.org/ssLists/schrec.do?school=a&sport=MBB
+# http://stats.ncaa.org/team/inst_team_list/10440?division=1
 
-h = Net::HTTP.new('web1.ncaa.org', 80)
+h = Net::HTTP.new('stats.ncaa.org', 80)
 teams = {}
-for s in "a".."z"
-  resp, data = h.get("/ssLists/schrec.do?school=#{s}&sport=MBB", nil )
+for s in 1..3
+  puts "Division #{s}"
+  resp, data = h.get("/team/inst_team_list/10440?division=#{s}", nil )
   data.each_line do |line|
-    if line =~ /\<option/
-      num = (line.scan /\d{1,}/)[0]
-      name = (line.scan /\>.+/)[0]
-      name = name[1..name.length]
-      
-      puts "Error alraedy exisits but different [#{teams[num]}] != #{name} (#{num})" if teams[num] && teams[num] != name
+    if line =~ /<a href="\/team\/index\/10440\?org_id=\d+/
+      num = (line.scan /\d{1,}/)[1]
+      name = (line.scan />.+</)[0]
+      name = name[1..name.length-2]
+
+      puts "Error already exists but different [#{teams[num]}] != #{name} (#{num})" if teams[num] && teams[num] != name
       teams[num] ||= name
-      #puts "#{num} - #{name}"
     end
   end
-  # puts "Code = #{resp.code}"
-  #   puts "Message = #{resp.message}"
-  #   resp.each {|key, val| printf "%-14s = %-40.40s\n", key, val }
-  #   p data[0..55]
 end
 
-f = File.open('/teams.txt', 'w')
+f = File.open(ARGV[0] || 'teams.txt', 'w')
 teams.each_pair {|num, name| puts "#{num}|#{name}"}
 teams.each_pair {|num, name| f.write "#{num}|#{name}"}
 f.close
