526bfa5d3709ff3d121cc337ff0df94045a0d4a0
Powell Kinney <pkinney@gmail.com>
2011-03-13 16:17:12 -0500

    Now using nokogiri for HTML parsing in gran_teams

--- grab_teams.rb
+++ grab_teams.rb
@@ -1,18 +1,21 @@
-require 'net/http'
+#!/usr/bin/ruby
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
 
 # http://stats.ncaa.org/team/inst_team_list/10440?division=1
 
-h = Net::HTTP.new('stats.ncaa.org', 80)
+#h = Net::HTTP.new('stats.ncaa.org', 80)
 teams = {}
 for s in 1..3
   puts "Division #{s}"
-  resp, data = h.get("/team/inst_team_list/10440?division=#{s}", nil )
-  data.each_line do |line|
-    if line =~ /<a href="\/team\/index\/10440\?org_id=\d+/
-      num = (line.scan /\d{1,}/)[1]
-      name = (line.scan />.+</)[0]
-      name = name[1..name.length-2]
-
+#  resp, data = h.get("/team/inst_team_list/10440?division=#{s}", nil )
+  doc = Nokogiri::HTML(open("http://stats.ncaa.org/team/inst_team_list/10440?division=#{s}"))
+  doc.css('a').each do |node|
+    if(node['href'] =~ /\/team\/index\/10440\?org_id=\d+/)
+      num = node['href'].scan(/\d+/)[1]
+      name = node.content
+#      puts name, num
       puts "Error already exists but different [#{teams[num]}] != #{name} (#{num})" if teams[num] && teams[num] != name
       teams[num] ||= name
     end
@@ -21,5 +24,7 @@ end
 
 f = File.open(ARGV[0] || 'teams.txt', 'w')
 teams.each_pair {|num, name| puts "#{num}|#{name}"}
-teams.each_pair {|num, name| f.write "#{num}|#{name}"}
+teams.each_pair {|num, name| f.write "#{num}|#{name}\n"}
 f.close
+
+puts "#{teams.size} teams loaded."
